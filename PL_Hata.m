575e54f50f0e0e1c036217b23386636d492fea24
Wangjiliang <wangjiliang1983@126.com>
2019-07-19 16:54:44 +0800

    Add file 1.4 and 1.5

---
+++ PL_Hata.m
@@ -0,0 +1,32 @@
+function [ PL ] = PL_Hata( fc,d,htx,hrx,Etype )
+%PL_HATA
+%input
+%    fc     : carrier frequency[Hz]
+%    d      : distance between base station and user[m]
+%    htx    : height of the tX[m]
+%    hrx    : height of the rx[m]
+%    Etype  : environment type('urban','suburban','open')
+%output
+%    PL     : sunhao [dB]
+if nargin <5
+    Etype = 'URBAN';
+end
+fc = fc/(1e6);
+if fc >= 150 && fc <= 200
+    C_Rx = 8.29*(log10(1.54*hrx))^2 - 1.1;
+elseif fc > 200
+    C_Rx = 3.2*(log10(11.75*hrx))^2 -4.97;
+else
+    C_Rx = 0.8+(1.1*log10(fc)-0.7)*hrx-1.56*log10(fc);
+end
+PL = 69.55 + 26.16*log10(fc)-13.82*log10(htx) -C_Rx ...
+    +(44.9-6.55*log10(htx))*log10(d/1000);
+EType=upper(Etype);
+if EType(1) == 'S'
+    PL = PL-2*(log10(fc/28))^2-5.4;
+elseif EType(1) == 'O'
+    PL = PL+(18.33-4.78*log10(fc))*log10(fc)-40.97;
+end
+
+end
+
